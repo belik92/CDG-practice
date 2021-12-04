aea915add65fa843658ba99a137b762b264ecf75
Konstantin Semochkin <mykatamaran@gmail.com>
2021-12-04 09:44:22 +0300

    Leave only test

--- task_2/foobar_spec.rb
+++ task_2/foobar_spec.rb
@@ -4,20 +4,7 @@
 #  в противном случае выводит сумму этих чисел.
 
 require 'rspec'
-def foobar
-  print "input first number "
-    first_number = gets.to_i
-  print "input second number "
-    second_number = gets.to_i
-  if first_number == 20
-      answer_1 = second_number
-  elsif second_number == 20
-      answer_2 = first_number     
-  else  summ = first_number + second_number
-      answer_3 = summ
-  end
-end
-puts foobar
+require './foobar.rb'
 
 # Тест программы
 RSpec.describe "defff" do
