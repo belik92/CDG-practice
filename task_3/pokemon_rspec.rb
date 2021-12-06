7fe849fb027dad7913b8e9e149d8df2d38842b7a
Konstantin Semochkin <mykatamaran@gmail.com>
2021-12-06 21:50:59 +0300

    Change indent

--- task_3/pokemon_rspec.rb
+++ task_3/pokemon_rspec.rb
@@ -1,16 +1,26 @@
+#Написать скрипт, который будет выводить массив покемонов
+#Спросит сколько добавить покемонов
+#Указанное на предыдущем этапе число раз спросит имя и цвет каждого покемона
+#Выведет в консоль массив содержащий хеши покемонов в формате 
+#`[{ name: 'Pikachu', color: 'Yellow' }, … n times{}]`
+#Код должен быть организован в методы
+
 require 'rspec'
 require './pokemon.rb'
+
 RSpec.describe "pokemon_family" do
-    it "#1_pokemon" do
-      allow_any_instance_of(Kernel).to receive(:gets).and_return(1, "pikachu", "yellow")
-      expect(pokemon).to eq([{name: "pikachu", color: "yellow"}])
-    end
-    it "#end_pokemon" do
-      allow_any_instance_of(Kernel).to receive(:gets).and_return(2, "pikachu", "green", "-1")
-      expect(pokemon).to eq([{name: "pikachu", color: "green"}])
-    end
-    it "#zero_pokemon" do
-      allow_any_instance_of(Kernel).to receive(:gets).and_return(0)
-      expect(pokemon).to eq("You have not any pokemons")
-    end
+  it "#1_pokemon" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(1, "pikachu", "yellow")
+    expect(pokemon).to eq([{name: "pikachu", color: "yellow"}])
   end
+
+  it "#end_pokemon" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(2, "pikachu", "green", "-1")
+    expect(pokemon).to eq([{name: "pikachu", color: "green"}])
+  end
+
+  it "#zero_pokemon" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return(0)
+    expect(pokemon).to eq("You have not any pokemons")
+  end
+end
--- task_3/string_rspec.rb
+++ task_3/string_rspec.rb
@@ -1,12 +1,17 @@
+#👉 Написать скрипт, который будет принимать слово, если слово заканчивается на “CS”
+#- выводит на экран цифру 2 в степени (длины введенного слова), если не заканчивается - выводит слово задом наперед
+
 require 'rspec'
 require './string_game.rb'
+
 RSpec.describe "convert_str" do
-    it "#1_convert" do
-      allow_any_instance_of(Kernel).to receive(:gets).and_return("pikachu")
-      expect(str_convert).to eq("uhcakip")
-    end
-    it "#1_double" do
-      allow_any_instance_of(Kernel).to receive(:gets).and_return("pikachuCS")
-      expect(str_convert).to eq(512)
-    end
+  it "#1_convert" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return("pikachu")
+    expect(str_convert).to eq("uhcakip")
   end
+
+  it "#1_double" do
+    allow_any_instance_of(Kernel).to receive(:gets).and_return("pikachuCS")
+    expect(str_convert).to eq(512)
+  end
+end
