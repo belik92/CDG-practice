ece983bc597179d1e0a8c50cb8aa7d0dd8b42da3
Konstantin Semochkin <mykatamaran@gmail.com>
2021-12-06 21:32:17 +0300

    change break if

--- task_3/pokemon.rb
+++ task_3/pokemon.rb
@@ -11,17 +11,17 @@ def pokemon
   i = gets.to_i
   if i > 0
     i.times do    
-    print "input pokemons name (put '-1' to exit) >> "
-    name = gets.chomp.to_s
-    break if name == "-1" 
-    print "input pokemons color(put '-1' to exit) >> "
-    color = gets.chomp.to_s
-    break if color == "-1"
+      print "input pokemons name (put '-1' to exit) >> "
+      name = gets.chomp.to_s
+        break if name == "-1" 
+        print "input pokemons color(put '-1' to exit) >> "
+      color = gets.chomp.to_s
+        break if color == "-1"
     #break if (name || color) == "-1" Если оставить это условие, и убрать строки 16 и 19. Прерывание будет работать неправильно
     pok_sample = {name: name, color: color} 
     pok_arr.push(pok_sample)
     end
-  pokemon = pok_arr  
+    pokemon = pok_arr  
   else pokemon = "You have not any pokemons"  
   end
 end
